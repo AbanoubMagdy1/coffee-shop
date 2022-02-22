c397b225ccbb5ab27819d40223b5d62ce910251d
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-02-22 13:18:07 +0200

    Add Head to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
+
 import Container from "../components/Container/Container";
 import HomeBanner from "../components/HomeBanner/HomeBanner";
 
 export default function Home(){
   return (
     <Container>
+      <Head>
+        <title>Find coffee shops</title>
+        <meta title="coffee shops"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
       <HomeBanner/>
     </Container>
   );
