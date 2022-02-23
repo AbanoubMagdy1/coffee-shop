6859d7a4188fc6e8453f0fa3966ea4a2ec24af38
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-02-23 14:55:22 +0200

    Add banner background

--- components/HomeBanner/HomeBanner.js
+++ components/HomeBanner/HomeBanner.js
@@ -1,21 +1,31 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./HomeBanner.module.scss";
 
 function HomeBanner() {
   console.log(styles);
   return (
     <section className={styles.banner}>
-      <h1 className={styles.banner_title}>
-        <span className={styles.title1}>Coffee</span>
+      <div className={styles.background}>
+        <Image
+          src="/home-background.png"
+          alt="home background"
+          layout="fill"
+        />
+      </div>
+      <div className={styles.main}>
+        <h1 className={styles.banner_title}>
+          <span className={styles.title1}>Coffee</span>
         &nbsp;
-        <span className={styles.title2}>Connisseur</span>
-      </h1>
+          <span className={styles.title2}>Connisseur</span>
+        </h1>
 
-      <p className={styles.banner_text}>
+        <p className={styles.banner_text}>
           Here you can find you favourite coffee shops
-      </p>
+        </p>
 
-      <button className="btn btn-purple">View shops nearby</button>
+        <button className={`btn btn-purple ${styles.btn}`}>View shops nearby</button>
+      </div>
     </section>
   );
 }
