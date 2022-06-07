ace33214a4266be5dcb3274d14c4a0ab8d07392d
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-06-07 22:44:54 +0200

    Add Foursquares api to query coffee stores and use it

--- components/HomeCard/HomeCard.js
+++ components/HomeCard/HomeCard.js
@@ -3,15 +3,36 @@ import Image from "next/image";
 import clsx from "clsx";
 import styles from "./HomeCard.module.scss";
 import Link from "next/link";
+import axios from "axios";
+import useSWR from "swr";
 
+async function getPlacePhoto(url){
+  console.log(process.env.FOURSQUARES_API);
+  return await axios.get(url, {
+    headers: {
+      Authorization: process.env.NEXT_PUBLIC_FOURSQUARES_API
+    }, params: {
+      limit: 1
+    }
+  });
+}
+
+function getPhotoUrl(data){
+  if(data){
+    return data.prefix + "original"+ data.suffix;
+  }
+}
 
 function HomeCard({ store }) {
+  const { data } = useSWR(`https://api.foursquare.com/v3/places/${store.fsq_id}/photos`, getPlacePhoto);
+  const imgUrl = getPhotoUrl(data?.data?.[0]) || "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FmZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60";
+
   return (
     <Link href={`/${store.id}`} passHref={true}>
       <a>
         <div className={clsx("glass", styles.homeCard)}>
           <Image
-            src={store.imgUrl}
+            src={imgUrl}
             alt={store.name}
             layout="responsive"
             width={1}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import HomeBanner from "../components/HomeBanner/HomeBanner";
 import HomeCard from "../components/HomeCard/HomeCard";
 
 import coffeeStores from "../public/coffee-stores";
+import { asyncHandler } from "../utils/async";
+import axios from "axios";
 
 
 export default function Home({ coffeeStores }){
@@ -20,17 +22,31 @@ export default function Home({ coffeeStores }){
         <h3 className="heading-secondary">Common coffee stores</h3>
       </div>
       <div className="home-cards">
-        {coffeeStores.map(store => <HomeCard key={store.id} store={store}/>)}
+        {coffeeStores.map(store => <HomeCard key={store.fsq_id} store={store}/>)}
       </div>
 
     </Container>
   );
 }
-
+//fsq34I0wfJWAGUomJDmGbd18OjQWv3Yn3MD2L8OmL4vYgSQ=
 export async function getStaticProps(){
+  const [res] = await asyncHandler(getCoffeeStores);
   return {
     props: {
-      coffeeStores
+      coffeeStores: res ? res.data.results : coffeeStores
     }
   };
+}
+
+async function getCoffeeStores(){
+  return await axios.get("http://api.foursquare.com/v3/places/search", {
+    headers: {
+      Authorization: process.env.NEXT_PUBLIC_FOURSQUARES_API
+    }, params: {
+      query: "coffee shop",
+      //near: "NYC",
+      ll: "30.075082866742452,31.264988029829123",
+      limit: 6
+    } }
+  );
 }
